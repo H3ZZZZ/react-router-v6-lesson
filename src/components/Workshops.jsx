faa40c70d5c2495678e26edbd45253f57c99a108
Frederik Hess <cph-fp103@cphbusiness.dk>
2022-10-31 15:52:30 +0100

    lesson 1 done

--- src/components/Workshops.jsx
+++ src/components/Workshops.jsx
@@ -1,7 +1,16 @@
+import { getWorkshops } from "../api";
+
 export default function Workshops() {
+  const workshops = getWorkshops();
+
   return (
-      <div className="container">
-          <h1>Workshop Categories</h1>
-      </div>
+    <div className="container">
+      <h1>Workshop Categories</h1>
+      <ul className="workshops">
+        {workshops.map((workshop) => (
+          <li key={workshop.id}>{workshop.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
