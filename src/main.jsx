4659ce443118a3cbd9951710e130b5ceaf4db688
Frederik Hess <cph-fp103@cphbusiness.dk>
2022-10-31 16:00:16 +0100

    lesson 2 half way done

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,47 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import logo from "../src/assets/schoolhacks-logo.png";
-import './styles/main.css'
+import "./styles/main.css";
 import Home from "./components/Home.jsx";
 import Workshops from "./components/Workshops";
+import About from "./components/About";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter>
-        <div className="main">
-            <header className="container">
-                <img
-                    className="logo"
-                    src={logo}
-                    alt="SchoolHacks logo"
-                    title="SchoolHacks | Home"
-                />
-                <nav>
-                    {/*Link*/}
-                </nav>
-            </header>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <div className="main">
+        <header className="container">
+          <img
+            className="logo"
+            src={logo}
+            alt="SchoolHacks logo"
+            title="SchoolHacks | Home"
+          />
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/workshops">Workshops</Link>
+            <Link to="/about">About</Link>
+          </nav>
+        </header>
 
-            <Routes>
-                <Route path="/" element={<Home titel="Welcome to SchoolHacks"/>}/>
-                <Route path="/workshops" element={<Workshops/>}/>
-            </Routes>
+        <Routes>
+          <Route path="/" element={<Home titel="Welcome to SchoolHacks" />} />
+          <Route path="/workshops" element={<Workshops />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
 
-            <footer className="container">
-                &copy;2022 | <a
-                href="https://www.schoolhacks.eu"
-                target="_blank"
-                rel="noreferrer noopener"
-            >SchoolHacks</a>
-            </footer>
-        </div>
-        </BrowserRouter>
-    </React.StrictMode>
-)
+        <footer className="container">
+          &copy;2022 |{" "}
+          <a
+            href="https://www.schoolhacks.eu"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            SchoolHacks
+          </a>
+        </footer>
+      </div>
+    </BrowserRouter>
+  </React.StrictMode>
+);
